876e331a4cdbd5d9e7b9755b19bc32b8a2471daa
Shane <shaneabh777@gmail.com>
2024-04-08 21:04:20 -0400

    Added data

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:fashion_ecommerce_project/products_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,30 +10,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  ThemeData _buildTheme(brightness) {
+    var baseTheme = ThemeData(brightness: brightness);
+
+    return baseTheme.copyWith(
+      textTheme: GoogleFonts.poppinsTextTheme(baseTheme.textTheme),
+    );
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: _buildTheme(Brightness.light),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
--- lib/products_page.dart
+++ lib/products_page.dart
@@ -19,74 +19,77 @@ class Product {
 class ProductsPage extends StatelessWidget {
   final List<Product> products = [
     Product(
-      name: 'Product 1',
-      price: 19.99,
-      rating: 4.5,
-      description: 'Description of Product 1',
-      imageUrl: ['images/black_hoodie.webp'],
-    ),
-    Product(
-      name: 'Product 2',
+      name: 'Loose Fit Hoodie',
       price: 29.99,
-      rating: 3.8,
-      description: 'Description of Product 2',
-      imageUrl: ['images/black_hoodie.webp'],
-    ),
-    Product(
-      name: 'Product 3',
-      price: 19.99,
       rating: 4.5,
-      description: 'Description of Product 1',
+      description:
+          'Loose-fit sweatshirt hoodie in medium weight cotton-blend fabric with a generous, but not oversized silhouette. Jersey-lined, drawstring hood, dropped shoulders, long sleeves, and a kangaroo pocket. Wide ribbing at cuffs and hem. Soft, brushed inside.',
       imageUrl: ['images/black_hoodie.webp'],
     ),
     Product(
-      name: 'Product 4',
-      price: 29.99,
+      name: 'Loose Fit Sweatshirt',
+      price: 14.99,
       rating: 3.8,
-      description: 'Description of Product 2',
-      imageUrl: ['images/black_hoodie.webp'],
+      description:
+          'Loose-fit sweatshirt in lightweight, cotton-blend fabric with a generous, but not oversized silhouette. Round, ribbed neck, dropped shoulders, and long sleeves. Wide ribbing at cuffs and hem. Soft, brushed inside.',
+      imageUrl: ['images/loose_fit_sweatshirt_1.jpeg','images/loose_fit_sweatshirt_2.jpeg', 'images/loose_fit_sweatshirt_3.jpeg'],
     ),
     Product(
-      name: 'Product 5',
-      price: 19.99,
+      name: 'Regular Fit Twill Shirt',
+      price: 37.99,
       rating: 4.5,
-      description: 'Description of Product 1',
-      imageUrl: ['images/black_hoodie.webp'],
+      description:
+          'Regular-fit shirt in soft cotton twill with a comfortable, classic silhouette. Turn-down collar, classic button placket, an open chest pocket, and yoke at back. Long sleeves, sleeve placket with link button, and button at cuffs. Rounded hem.',
+      imageUrl: ['images/regular_fit_twill_shirt_1.jpeg','images/regular_fit_twill_shirt_2.jpeg','images/regular_fit_twill_shirt_3.jpeg'],
     ),
     Product(
-      name: 'Product 6',
-      price: 29.99,
+      name: 'Relaxed Fit Flannel Shirt',
+      price: 37.99,
       rating: 3.8,
-      description: 'Description of Product 2',
-      imageUrl: ['images/black_hoodie.webp'],
+      description: 'Relaxed-fit shirt in soft cotton flannel. Turn-down collar, buttons at front, and yoke at back. Open chest pocket, long sleeves, sleeve placket with link button, and button at cuffs. Gently rounded hem.',
+      imageUrl: ['images/relaxed_fit_flannel_shirt_1.jpeg','images/relaxed_fit_flannel_shirt_2.jpeg','images/relaxed_fit_flannel_shirt_3.webp'],
     ),
     Product(
-      name: 'Product 7',
-      price: 19.99,
+      name: 'Loose Fit Lyocell Resort Shirt',
+      price: 31.99,
       rating: 4.5,
-      description: 'Description of Product 1',
-      imageUrl: ['images/black_hoodie.webp'],
+      description: 'Loose-fit shirt in woven lyocell with a generous but not oversized silhouette. Resort collar, buttons without placket, and short sleeves. Open chest pocket, yoke at back, and a straight-cut hem with a small slit at sides. Fabric made from lyocell is super soft, wrinkle resistant, and nicely draped.',
+      imageUrl: ['images/loose_fit_lyocell_resort_shirt_1.jpeg', 'images/loose_fit_lyocell_resort_shirt_2.jpeg', 'images/loose_fit_lyocell_resort_shirt_3.jpeg'],
     ),
     Product(
-      name: 'Product 8',
-      price: 29.99,
+      name: 'Loose Jeans',
+      price: 64.99,
       rating: 3.8,
-      description: 'Description of Product 2',
-      imageUrl: ['images/black_hoodie.webp'],
+      description: '5-pocket jeans in non-stretch cotton denim. Rounded legs and loose fit from seat to hem for extra room around legs. Regular waist, zip fly, and dropped gusset. Perfect for the full denim look.',
+      imageUrl: ['images/loose_jeans_1.jpeg', 'images/loose_jeans_2.jpeg','images/loose_jeans_3.jpeg'],
     ),
     Product(
-      name: 'Product 9',
-      price: 19.99,
+      name: 'Slim Jeans',
+      price: 35.99,
       rating: 4.5,
-      description: 'Description of Product 1',
-      imageUrl: ['images/black_hoodie.webp'],
+      description: 'Straight-leg, 5-pocket jeans in cotton denim with gentle stretch for added comfort. Slim fit from waist through thighs. Regular waist and zip fly. Easily styled for a sleek or sporty look.',
+      imageUrl: ['images/slim_jeans_1.jpeg','images/slim_jeans_2.jpeg','images/slim_jeans_3.jpeg'],
     ),
     Product(
-      name: 'Product 10',
-      price: 29.99,
+      name: 'Slim Fit Fine-knit Cotton Sweater',
+      price: 37.99,
       rating: 3.8,
-      description: 'Description of Product 2',
-      imageUrl: ['images/black_hoodie.webp'],
+      description: 'Slim-fit sweater in soft, fine-knit cotton. Crew neck, long sleeves, and ribbing at neck, cuffs, and hem.',
+      imageUrl: ['images/slim_fit_fine_knit_cotton_sweater_1.jpeg','images/slim_fit_fine_knit_cotton_sweater_2.jpeg','images/slim_fit_fine_knit_cotton_sweater_3.jpeg'],
+    ),
+    Product(
+      name: 'Slim Fit Half-zip Sweater',
+      price: 54.99,
+      rating: 4.5,
+      description: 'Slim-fit, knit sweater in a soft cotton blend with a fitted silhouette. Stand-up collar, half-zip, long sleeves, and a straight hem',
+      imageUrl: ['images/slim_fit_half_zip_sweater_1.jpeg','images/slim_fit_half_zip_sweater_2.jpeg','images/slim_fit_half_zip_sweater_3.jpeg'],
+    ),
+    Product(
+      name: 'Oversized Fit Sweater',
+      price: 58.99,
+      rating: 3.8,
+      description: 'Oversized sweater in a soft knit with alpaca wool content. Crew neck, dropped shoulders, and long sleeves. Wide ribbing at neck, cuffs, and hem.',
+      imageUrl: ['images/oversized_fit_sweater_1.jpeg','images/oversized_fit_sweater_2.jpeg','images/oversized_fit_sweater_3.jpeg'],
     ),
     // Add more products as needed
   ];
@@ -120,16 +123,27 @@ class ProductsPage extends StatelessWidget {
                           children: [
                             const Text("Get your special sale up to 50%",
                                 style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 24,
-                                )),
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold)),
                             ElevatedButton(
-                                onPressed: () {}, child: const Text("Shop Now"))
+                                style: ElevatedButton.styleFrom(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 15, vertical: 5),
+                                ),
+                                onPressed: () {},
+                                child: const Text(
+                                  "Shop Now",
+                                  style: TextStyle(
+                                      color: Color(0xFFFA6E20),
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold),
+                                ))
                           ],
                         ),
                       ),
                       Expanded(
-                          flex: 1,
+                          flex: 2,
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
@@ -149,7 +163,7 @@ class ProductsPage extends StatelessWidget {
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.60,
                 width: MediaQuery.of(context).size.width,
-                child:Container(
+                child: Container(
                   margin: EdgeInsets.only(left: 10, right: 10),
                   child: CustomScrollView(
                     slivers: <Widget>[
@@ -158,10 +172,11 @@ class ProductsPage extends StatelessWidget {
                         sliver: SliverGrid.count(
                           crossAxisCount: 2,
                           crossAxisSpacing: 10,
-                          mainAxisSpacing: 10,
+                          mainAxisSpacing: 30,
                           children: List.generate(
                             products.length,
-                                (index) => GridTile(
+                            (index) => GridTile(
+
                               child: Card(
                                 color: const Color(0xFFFFFBFF),
                                 child: Column(
@@ -169,32 +184,32 @@ class ProductsPage extends StatelessWidget {
                                   children: [
                                     SizedBox(
                                       width: double.infinity,
-                                      height: 90,
-                          
+                                      height: 80,
                                       child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(8.0),
+                                        borderRadius:
+                                            BorderRadius.circular(8.0),
                                         child: Image.asset(
                                           products[index].imageUrl[0],
                                           fit: BoxFit.contain,
-
                                         ),
                                       ),
                                     ),
                                     Padding(
                                       padding: const EdgeInsets.all(8.0),
                                       child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
                                         children: [
                                           Text(
                                             products[index].name,
-                                            style: TextStyle(
+                                            style: const TextStyle(
                                               fontWeight: FontWeight.w400,
-                                              fontSize: 18,
+                                              fontSize: 16,
                                             ),
                                           ),
                                           Text(
                                             '\$${products[index].price.toString()}',
-                                            style: TextStyle(
+                                            style: const TextStyle(
                                               fontWeight: FontWeight.bold,
                                               fontSize: 15,
                                             ),
@@ -212,8 +227,6 @@ class ProductsPage extends StatelessWidget {
                     ],
                   ),
                 ),
-
-
               )
             ],
           ),
